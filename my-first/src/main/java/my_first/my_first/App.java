6c89de95c32c8c216a9b4eeabef5d22ce7115e97
hp <hp@192.168.1.4>
2018-02-09 20:31:15 +0000

    hind

---
+++ my-first/src/main/java/my_first/my_first/App.java
@@ -0,0 +1,13 @@
+package my_first.my_first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
