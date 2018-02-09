87c8fcb1fd051be1313a038877fbe11378b8c8ca
hp <hp@192.168.1.4>
2018-02-09 20:53:08 +0000

    hhh

--- my-first/src/main/java/my_first/my_first/App.java
+++ my-first/src/main/java/my_first/my_first/App.java
@@ -6,7 +6,7 @@ package my_first.my_first;
  */
 public class App 
 {
-    public static void main( String[] args )//
+    public static void main( String[] args )//66
     {
     	
         System.out.println( "Hello World!" );
