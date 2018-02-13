c2e7216bce5b42afefecebe1d3a03a9672b66c81
hp <hp@192.168.1.8>
2018-02-13 21:34:53 +0000

    gg

--- my-first/src/main/java/model/Author.java
+++ my-first/src/main/java/model/Author.java
@@ -20,15 +20,44 @@ import java.io.Serializable;
 		@UniqueConstraint(columnNames = "LAST_NAME") })
 
 public class Author implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	Long id;
 private String firstName;
 private String LastName;
+private Set<Book> categories = new HashSet<Book>(0);
+
+@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+@JoinTable(name = "Book_Author", catalog = "mkyongdb", joinColumns = {
+		@JoinColumn(name = "AUTHOR_ID", nullable = false, updatable = false) },
+		inverseJoinColumns = { @JoinColumn(name = "BOOK_ID",
+				nullable = false, updatable = false) })
+public Set<Book> getCategories() {
+	return categories;
+}
+public void setCategories(Set<Book> categories) {
+	this.categories = categories;
+}
+public Author() {
+	super();
+}
+public Author(String firstName, String lastName, Set<Book> categories) {
+	super();
+	this.firstName = firstName;
+	LastName = lastName;
+	this.categories = categories;
+}
+@Column(name = "FIRST_NAME", unique = true, nullable = false, length = 20)
+
 public String getFirstName() {
 	return firstName;
 }
 public void setFirstName(String firstName) {
 	this.firstName = firstName;
 }
+@Column(name = "LAST_NAME", unique = true, nullable = false, length = 10)
 public String getLastName() {
 	return LastName;
 }
@@ -40,6 +69,9 @@ public Author(String firstName, String lastName) {
 	this.firstName = firstName;
 	LastName = lastName;
 }
+@Id
+@GeneratedValue(strategy = IDENTITY)
+@Column(name = "Author_ID", unique = true, nullable = false)
 public Long getId() {
 	return id;
 }
